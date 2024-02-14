313270d2a10657db8b1af637644e6c5d3e1632c5
idctag <idctag@Battulgas-MacBook-Air.local>
2024-02-14 12:01:23 +0900

    setup

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,4 @@
-use std::{fs, usize};
-
 fn main() {
-    let input = fs::read_to_string("input.txt").unwrap();
-    let v = count_sum(&input as &str);
-    println!("{:?}", v)
+    advent::day01::day_one()
 }
 
-fn count_sum(input: &str) -> usize {
-    let lines: Vec<&str> = input.rsplit("\n").collect();
-    let mut line_nums: Vec<usize> = vec![];
-    for line in lines {
-        let mut buffer = String::new();
-        for c in line.chars() {
-            match c.to_string().parse::<usize>() {
-                Ok(_) => buffer.push(c),
-                Err(_) => (),
-            }
-        }
-        if buffer.len() > 2 {
-            buffer = format!(
-                "{}{}",
-                buffer.chars().next().unwrap(),
-                buffer.chars().last().unwrap()
-            )
-        } else if buffer.len() == 1 {
-            buffer = format!("{}{}", buffer, buffer)
-        }
-        match buffer.to_string().parse::<usize>() {
-            Ok(n) => line_nums.push(n),
-            Err(_) => (),
-        }
-    }
-    line_nums.iter().sum()
-}
